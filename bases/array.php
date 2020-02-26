7e9dcf31c612d4d380654220d449eef1fc29998c
Vincent Letourneau <vincent@nanoninja.com>
2020-02-26 15:22:15 +0100

    Add array menu

--- bases/array.php
+++ bases/array.php
@@ -1,1 +1,58 @@
 <?php
+
+// Tableau à index numérique
+// $arr = ['a', 'b', 'c'];
+
+// $arr[1] = 'a';
+// $arr[34] = 'b';
+// $arr['1.5'] = 'c';
+// $arr['name'] = 'toto';
+// $arr[] = 'test';
+
+// Tableau associatif
+// $arr = [
+//     'username' => 'toto',
+//     'password' => '0000',
+//     1 => 'hello',
+//     'funk' => function () {
+//         echo 'Get Funky !!!';
+//     },
+// ];
+
+// $arr['age'] = 20;
+// $id = 1;
+
+$arr = [
+    [
+        'fruits' => [[[[7]]]],
+        'Carotte', array('a', [
+            'b'
+        ], 'c', 'd'),
+        'name' => [
+    'ville' => [
+        'master' => 'Trivette'],
+    'New York',
+    ],
+]];
+
+// echo $arr[0]['name']['ville']['master'];
+
+// $users[] = [
+//     'username' => 'trivette',
+// ];
+
+// $users[] = [
+//     'username' => 'Steven Seagle',
+// ];
+
+// echo $users[1]['username'];
+
+// $users = [
+//     0 => [
+//         'username' => 'SteveSeagle'
+//     ],
+//     1 => [
+//         'username' => 'Chuck le titan'
+//     ]
+// ];
+
---
+++ menu/menu.php
@@ -0,0 +1,28 @@
+<?php
+
+return [ // ul
+    [ // li
+        [ // a
+            'link' => 'https://google.com',
+            'name' => 'Google'
+        ],
+        [ // a
+            'link' => 'https://bing.com',
+            'name' => 'Bing'
+        ]
+    ],
+    [ // li
+        [ // a
+            'link' => 'https//aol.com',
+            'name' => 'Aol',
+            'menu' => [ // ul
+                [ // li
+                    [ // a
+                        'link' => 'https://lycos.com',
+                        'name' => 'Lycos'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
